bb9f24909336e603880cd9f0910d250436729950
Zhu <wang@example.com>
2026-02-11 18:20:57 +0800

    feat:添加分类模块

---
+++ src/category.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+class Category:
+    def _init_(self, name):
+        self.name = name
+        self.article = []
+
+     def add_article(self, title):
+         self.article.append(title)
+         print(f"文章《{title}》已添加“)
